af2500dc1a22c339368294e7caf2559b1cdfc137
yunseung <yseungun.21@gmail.com>
2024-01-01 23:30:18 +0900

    init: 환경설정

---
+++ src/main/java/study/studyspring1/StudySpring1Application.java
@@ -0,0 +1,12 @@
+package study.studyspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpring1Application.class, args);
+	}
+}
---
+++ src/test/java/study/studyspring1/StudySpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.studyspring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
