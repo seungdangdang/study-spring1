6a05bbf958f1ecf59d7da01d18f55c2ae6bfe92c
yunseung <yseungun.21@gmail.com>
2024-01-01 23:29:32 +0900

    chore: 설명 추가

--- src/main/java/study/studyspring1/controller/HelloController.java
+++ src/main/java/study/studyspring1/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     // GetMapping = 주소창에 /hello가 들어오면 이 메서드를 호출함
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello!!"); //속성의 이름: data, 속성의 값: hello
-        return "hello"; // return 한 hello ~> hello.html을 찾아감
+        model.addAttribute("data", "반갑습니다."); //속성의 이름: data, 속성의 값: hello
+        return "hello"; // springboot의 'viewResolver'가 return된 hello를 확인하고 resources -> templates에서 hello.html을 찾아감
     }
 }
