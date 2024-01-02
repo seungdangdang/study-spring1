073ebc0fb94ff1dd3e75c8788dc07fc0335077f6
yunseung <yseungun.21@gmail.com>
2024-01-02 23:18:47 +0900

    feat: Api를 사용하는 코드 작성

--- src/main/java/study/studyspring1/controller/HelloController.java
+++ src/main/java/study/studyspring1/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
@@ -22,4 +23,32 @@ public class HelloController {
         model.addAttribute("name3", name2); // key, value
         return "hello-template";
     } // 이 코드를 실행하기 위해 주소창에는 http://localhost:8080/hello-mvc?name2=블라블라
+
+    // API를 배우면서(1) - 단순 문자를 요구할 때 쓰며 쓸일은 거의 없음, html없이 문자그대로 내려가는 방식
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name2) {
+        return "hello" + name2;
+    }
+
+    // API 배우면서(2) - 문자가 아닌 데이터를 요구할 떄
+    @GetMapping("hello-api")
+    @ResponseBody // defalut 반환 json 방식
+    public Hello helloApi(@RequestParam("name") String name2) {
+        Hello hello = new Hello();
+        hello.setName(name2);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
