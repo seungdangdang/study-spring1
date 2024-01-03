87102513dc96361f4eda5a2628ff70154d624f96
yunseung <yseungun.21@gmail.com>
2024-01-04 01:15:13 +0900

    feat: 회원가입 컨트롤러 작성

--- src/main/java/study/studyspring1/controller/MemberController.java
+++ src/main/java/study/studyspring1/controller/MemberController.java
@@ -1,7 +1,9 @@
 package study.studyspring1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.studyspring1.domain.Member;
 import study.studyspring1.service.MemberService;
 
 @Controller
@@ -11,5 +13,18 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getInputValue());
+        memberService.join(member);
+        return "redirect:/";
     }
 }
