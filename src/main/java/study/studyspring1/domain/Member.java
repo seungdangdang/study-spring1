d784c8dc6a0c093581340cccd4f5d2553bcc24ee
yunseung <yseungun.21@gmail.com>
2024-01-03 21:50:31 +0900

    feat: Member클래스 생성

---
+++ src/main/java/study/studyspring1/domain/Member.java
@@ -0,0 +1,23 @@
+package study.studyspring1.domain;
+
+public class Member {
+
+    private Long id; // 시스템이 정하는 id
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
