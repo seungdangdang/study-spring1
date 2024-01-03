bd71c32902228b5620f645f260c611274e7b85c3
yunseung <yseungun.21@gmail.com>
2024-01-04 00:35:22 +0900

    delete: Component 애노테이션 삭제
    - 스프링 빈 직접 등록해보기 위함
    - repository
    - service

--- src/main/java/study/studyspring1/controller/MemberController.java
+++ src/main/java/study/studyspring1/controller/MemberController.java
@@ -9,7 +9,6 @@ public class MemberController {
 
     private MemberService memberService;
 
-    @Autowired // 생성자에 Autowired를 쓰면 멤버서비스를 스프링컨테이너에 있는 멤버 서비스를 가져다가 연결을 해줌
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
--- src/main/java/study/studyspring1/repository/MemoryMemberRepository.java
+++ src/main/java/study/studyspring1/repository/MemoryMemberRepository.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 import org.springframework.stereotype.Repository;
 import study.studyspring1.domain.Member;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
