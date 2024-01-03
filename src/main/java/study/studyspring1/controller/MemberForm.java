3e226a4920ca6900087730e852079e45d40b2bcf
yunseung <yseungun.21@gmail.com>
2024-01-04 01:15:23 +0900

    feat: 회원 이름 입력 값 관련 클래스 작성

---
+++ src/main/java/study/studyspring1/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package study.studyspring1.controller;
+
+public class MemberForm {
+    private String inputValue;
+
+
+    public String getInputValue() {
+        return inputValue;
+    }
+
+    public void setInputValue(String inputValue) {
+        this.inputValue = inputValue;
+    }
+}
