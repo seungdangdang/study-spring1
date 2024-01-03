1d3ce3a319f225e17ca68a300ccf7cf1130aca88
yunseung <yseungun.21@gmail.com>
2024-01-03 22:13:49 +0900

    feat: 저장소 관련 클래스 생성

---
+++ src/main/java/study/studyspring1/repository/MemoryMemberRepository.java
@@ -0,0 +1,41 @@
+// 저장소: 메모리일 때
+
+package study.studyspring1.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import study.studyspring1.domain.Member;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); // 스토어에 저장하기 전에 시퀀스를 id에 세팅
+        store.put(member.getId(), member); // 스토어에 id(key)와 name(value) 받기
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); // 끝까지 돌렸는데 못찾으면 Optional에 null이 포함돼서 반환이 됨
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
