cfd3e490a464b9daa57cf20485fc47af5fc0ad0a
yunseung <yseungun.21@gmail.com>
2024-01-04 21:41:54 +0900

    feat: 회원목록 조회 컨트롤러 작성

--- src/main/java/study/studyspring1/controller/MemberController.java
+++ src/main/java/study/studyspring1/controller/MemberController.java
@@ -1,6 +1,8 @@
 package study.studyspring1.controller;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import study.studyspring1.domain.Member;
@@ -26,5 +28,12 @@ public class MemberController {
         member.setName(form.getInputValue());
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
