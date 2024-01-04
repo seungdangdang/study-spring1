e9e2c4f4b31b812eb9980996484855c8122e40bc
yunseung <yseungun.21@gmail.com>
2024-01-04 23:49:09 +0900

    feat: JDBC repository 로 변경

--- src/main/java/study/studyspring1/service/SpringConfig.java
+++ src/main/java/study/studyspring1/service/SpringConfig.java
@@ -2,13 +2,23 @@
 
 package study.studyspring1.service;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import study.studyspring1.repository.JdbcMemberRepository;
 import study.studyspring1.repository.MemberRepository;
 import study.studyspring1.repository.MemoryMemberRepository;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
